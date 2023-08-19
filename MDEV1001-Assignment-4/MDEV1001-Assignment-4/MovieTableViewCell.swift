b6600d733d2715f034127557d7e51ea4dd1831e2
GurwinderSinghMDev <133176884+GurwinderSinghMDev@users.noreply.github.com>
2023-08-18 23:46:06 -0400

    Project files for assignment 4

---
+++ MDEV1001-Assignment-4/MDEV1001-Assignment-4/MovieTableViewCell.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+  
+    @IBOutlet weak var posterImageView: UIImageView!
+  
+    @IBOutlet weak var titleLabel: UITextField!
+  
+    @IBOutlet weak var studioLabel: UITextField!
+   
+    @IBOutlet weak var ratingLabel: UITextField!
+}
+
